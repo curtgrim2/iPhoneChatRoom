ba94bc65a898fcfdea711f31a398cf2463ad4174
curtgrim2 <59848823+curtgrim2@users.noreply.github.com>
2024-03-05 12:45:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var http = require('http');
+var fs = require('fs');
 
 var app = express();
 app.use(express.static('public', {
-  setHeaders: (res, path, stat) => {
+  setHeaders: (res, path, stat) => { //Allows CSS to conenct to HTML file
     if (path.endsWith('.css')) {
       res.set('Content-Type', 'text/css');
     }
@@ -23,22 +24,35 @@ app.get('/', (req, res) => {
 
 
 var name;
+var totalclients = 0;
+const defaultpropic = fs.readFileSync("C:\\Users\\dgrea\\TestChatRoom\\public\\propic3.png");
 
 io.on('connection', (socket) => {
-  console.log('new user connected');
-  
+  //console.log('new user connected');
+  ++totalclients;
   socket.on('joining msg', (username) => {
+    
+    console.log(totalclients);
   	name = username;
-  	io.emit('chat message', `---${name} joined the chat---`);
+  	//io.emit('chat message', `---${name} joined the chat---`);
+    io.emit('getPropic1',{image:defaultpropic.toString('base64'),totalclients:totalclients});
   });
+
+
   
   socket.on('disconnect', () => { //triggers when page closes/refreshes
+    totalclients--;
     console.log('user disconnected');
-    io.emit('chat message', `---${name} left the chat---`);
-    
+
+    io.emit('chat message', `---${name} left the chat---`);   
   });
   socket.on('chat message', (msg) => {
     socket.broadcast.emit('chat message', msg);         //sending message to all except the sender
+  });
+
+
+  socket.on('defaultpropic', () => {
+   // socket.emit('getPropic1',{image:defaultpropic.toString('base64')});
   });
 });
 
